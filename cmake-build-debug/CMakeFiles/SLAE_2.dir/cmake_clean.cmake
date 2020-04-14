afaa919006b59657101a73aa3a026b95d2e316c5
PBal <pbal00@mail.ru>
2020-04-14 19:49:59 +0300

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/SLAE_2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SLAE_2.dir/main.c.obj"
+  "SLAE_2.exe"
+  "SLAE_2.exe.manifest"
+  "SLAE_2.pdb"
+  "libSLAE_2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SLAE_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
